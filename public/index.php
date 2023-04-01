10c4ffcfa6c023082fe49a68193cc317a044b392
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2023-04-01 10:54:12 +0530

    separate file for routing

--- public/index.php
+++ public/index.php
@@ -1,25 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Core\Application;
-use App\Controllers\ContactController;
-use App\Controllers\BlogController;
-use App\Controllers\AuthController;
-
-$app = new Application();
-
-$app->router->get('/',  function () {
-    return view('home')->layout('layouts.app')->render();
-});
-
-$app->router->match(['get', 'post'], '/login', [AuthController::class, 'loginPage']);
-$app->router->match(['get', 'post'], '/register', [AuthController::class, 'registerPage']);
-
-$app->router->get('/contact', [ContactController::class, 'getPage']);
-$app->router->post('/contact', [ContactController::class, 'submit']);
-
-
-$app->router->get('/blog/{slug}', [BlogController::class, 'index']);
-
-$app->run();
+require_once __DIR__ . '/../routes/web.php';
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Controllers\AuthController;
+use App\Controllers\BlogController;
+use App\Controllers\ContactController;
+use App\Core\Application;
+
+$app = new Application();
+
+$app->router->get('/',  function () {
+    return view('home')->layout('layouts.app')->render();
+});
+
+$app->router->match(['get', 'post'], '/login', [AuthController::class, 'loginPage']);
+$app->router->match(['get', 'post'], '/register', [AuthController::class, 'registerPage']);
+
+$app->router->get('/contact', [ContactController::class, 'getPage']);
+$app->router->post('/contact', [ContactController::class, 'submit']);
+
+$app->router->get('/blog/{slug}', [BlogController::class, 'index']);
+
+$app->run();
