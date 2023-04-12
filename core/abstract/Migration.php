b9f4f119b25f37cc0ca1e06a167f82fd3e62a0b7
prajwal89 <prajwalhallale89@gmail.com>
2023-04-12 12:55:25 +0530

    clean up

--- core/abstract/Migration.php
+++ core/abstract/Migration.php
@@ -8,30 +8,26 @@ abstract class Migration
 {
     public function run(string $sql)
     {
-
         try {
-            //* run migrations
             Database::connection()->exec($sql);
         } catch (\PDOException $e) {
             exit($e->getMessage());
         }
 
-        //todo find other way
+        //todo find efficient way 
         $backtrace = debug_backtrace();
         $caller = isset($backtrace[1]['function']) ? $backtrace[1]['function'] : 'unknown';
         $migrationName = basename(get_called_class());
 
         if ($caller == 'up') {
-            //* record migration record
+            //record migration
             $sql = "INSERT INTO migrations (migration) VALUES ('$migrationName')";
             Database::connection()->exec($sql);
         }
 
         if ($caller == 'down') {
-            //* remove migration record
-            //todo use bindings
-            $sql = "DELETE FROM migrations WHERE migration = '$migrationName' ";
-            Database::connection()->exec($sql);
+            //remove migration record
+            Database::connection()->exec("DELETE FROM migrations WHERE migration = '$migrationName'");
         }
 
         if ($caller == 'unknown') {
